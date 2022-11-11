b78a13178d594cd82fc1f105afefc778c590d9df
PabasaraPalihena <pabapalihena@gmail.com>
2022-11-11 12:30:00 +0530

    modify the package.json

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
 const https = require("https"),
   fs = require("fs");
 
 const options = {
-  key: fs.readFileSync("server-ket.pem"),
+  key: fs.readFileSync("server-key.pem"),
   cert: fs.readFileSync("server-cert.pem"),
 };
 
-const app = express();
+//request allow any domain
+app.use(cors({ origin: "*" }));
+
+//Body parser
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+app.use(bodyParser.json());
 
 app.use((req, res) => {
   res.writeHead(200);
@@ -16,3 +27,5 @@ app.use((req, res) => {
 app.listen(8000);
 
 https.createServer(options, app).listen(8080);
+
+module.exports = app;

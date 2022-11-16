996d2506b1e83b940301bffb392ebd5b97aaf244
PabasaraPalihena <pabapalihena@gmail.com>
2022-11-16 08:50:06 +0530

    encrypt and decrypt data

---
+++ server/encrypt.js
@@ -0,0 +1,15 @@
+const crypto = require("crypto-js");
+
+var data = "This is the data that need to be encrypted";
+
+// Encrypte the data
+var encrypted = crypto.AES.encrypt(data, key).toString();
+console.log("Encrypted data");
+
+// Printing the encrypted data
+console.log(encrypted);
+console.log("Decrypted data");
+
+// Decrypting the data
+var decrypted = crypto.AES.decrypt(encrypted, key).toString(crypto.enc.Utf8);
+console.log(decrypted);
